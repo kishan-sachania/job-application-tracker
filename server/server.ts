c7c366d9448909c533c4713de7f7afb46ffa8b8e
Kishan-Sachania <kishansachania2003@gmail.com>
2026-08-14 16:03:59 +0530

    Update:Set up health api to mpnitor render deployment

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,16 @@
 import app from "./app/app.js";
 import config from "./config/config.js";
 import mongoConnection from "./config/mongo-connection.js";
+import ApiResponse from "./utils/api-response.js";
 
 const PORT = config.PORT;
 
 const startServer = async () => {
     // Initiate MongoDB Connection
     await mongoConnection();
+
+    // Health Check API
+    app.get("/", (req, res) => { ApiResponse.success(res, "Health API Hit", 200) })
 
     // Start Express app server
     app.listen(PORT, () => {
